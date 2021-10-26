528c68245f04eb87a661e290498c0a4c0f509876
ruhulaminjr <ruhultuber@gmail.com>
2021-10-27 00:02:37 +0600

    added some change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT ||  5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -42,6 +42,9 @@ async function run() {
       const cardProducts = await productsCollection.find(query).toArray();
       console.log(cardProducts);
       res.json(cardProducts);
+    });
+    app.get("/", (req, res) => {
+      res.send('<h2>Server Running Successfully</h2>');
     });
   } finally {
   }
